74110687ab1ab999e420a748627b4efdab718009
Manuel Kersic <manuel.kersic@posteo.de>
2023-07-26 23:37:35 +0200

    improve events_list2df function

--- src/scraper_collection.py
+++ src/scraper_collection.py
@@ -15,27 +15,19 @@ class ScraperCollection():
     def get_scraper_names(self) -> list[str]:
         return list(self.scrapers.keys())
     
-    def get_all_events(self, end_date: dt.datetime, start_date: dt.datetime = dt.datetime.now(),**kwargs) -> pd.DataFrame:
+    def get_all_events_df(self, end_date: dt.datetime, start_date: dt.datetime = dt.datetime.now(),**kwargs) -> pd.DataFrame:
         events: list[Event] = []
         for _, scraper in self.scrapers.items():
             events += scraper.scrape_events(end_date=end_date,start_date=start_date)
-        return self.events2df(events)
+        events_df = events_list2df(events)
+        return events_df
     
-    @staticmethod
-    def events2df(events: list[Event]) -> pd.DataFrame:
-        df = pd.DataFrame({
-            'Start Date' : [event.start_date for event in events],
-            'End Date' :  [event.end_date for event in events],
-            'Title' : [event.title for event in events],
-            'Url': [event.url for event in events],
-            'Location': [event.location for event in events],
-            'Event Type' : [event.event_type for event in events],
-            'Description Short' : [event.description_short for event in events],
-            'Description Long' : [event.description_long for event in events],
-            'Organiser' : [event.organiser for event in events],
-            'Periodic' : [event.periodic for event in events],
-        })
-        return df
-
+def events_list2df(events: list[Event]) -> pd.DataFrame:
+    '''
+    Converts a list of Event objects into a DataFrame with a column for each Event field.
+    '''
+    fields = Event.__fields__.keys()
+    field_dict = {field: [getattr(event, field) for event in events] for field in fields}
+    return pd.DataFrame(field_dict)
 
 scraper_collection = ScraperCollection()
