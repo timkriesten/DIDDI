b3d0c2f11ae94c888f2f42362dfa36fb94a8b5e0
Manuel Kersic <manuel.kersic@posteo.de>
2023-09-04 22:44:17 +0200

    next steps for gui

--- src/scraper_collection.py
+++ src/scraper_collection.py
@@ -3,8 +3,8 @@ import pandas as pd
 import src.scrapers as scrapers
 from src.definitions import InputWebsiteScraper, Event
 
-# TODO: make an automatic import from all scraper clases
-global_scrapers_list: list[InputWebsiteScraper] = [
+# TODO: make an automatic import from all scraper classes
+scrapers_list: list[InputWebsiteScraper] = [
         scrapers.JapanischesPalais(),
         scrapers.SLUB(),
         scrapers.LokaleAgenda(),
@@ -18,16 +18,16 @@ global_scrapers_list: list[InputWebsiteScraper] = [
         scrapers.Verbraucherzentrale()
     ]
 
-
 class ScraperCollection():
-    scrapers: dict[str, InputWebsiteScraper] = {scraper.name: scraper for scraper in global_scrapers_list}
-
-    def get_scraper_names(self) -> list[str]:
-        return list(self.scrapers.keys())
+    scrapers = scrapers_list
+    names = [scraper.name for scraper in scrapers]
+    urls = [scraper.url for scraper in scrapers]
+    ready = [scraper.ready for scraper in scrapers]
     
-    def get_all_events_df(self, end_date: dt.datetime, start_date: dt.datetime = dt.datetime.now(),**kwargs) -> pd.DataFrame:
+    @classmethod
+    def get_all_events_df(cls, end_date: dt.datetime, start_date: dt.datetime = dt.datetime.now(),**kwargs) -> pd.DataFrame:
         events: list[Event] = []
-        for _, scraper in self.scrapers.items():
+        for scraper in cls.scrapers:
             events += scraper.scrape_events(end_date=end_date,start_date=start_date)
         events_df = events_list2df(events)
         return events_df
@@ -39,5 +39,3 @@ def events_list2df(events: list[Event]) -> pd.DataFrame:
     fields = Event.__fields__.keys() # type: ignore
     field_dict = {field: [getattr(event, field) for event in events] for field in fields}
     return pd.DataFrame(field_dict)
-
-scraper_collection = ScraperCollection()
