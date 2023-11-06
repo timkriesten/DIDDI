059187b00b880e9120f65eb7d2a366a64ef651e9
Manuel Kersic <manuel.kersic@posteo.de>
2023-11-06 18:23:14 +0100

    final grid finalized

--- main.py
+++ main.py
@@ -53,9 +53,9 @@ date_and_scrapers = VBox([
 # date_and_scrapers
 
 # %%
-# generate grid_draft widget
+# generate draft grid widget
 
-column_defs = [{'headerName':'Start','field':'start_date', 'cellDataType': 'dateString', 'checkboxSelection': True},
+draft_column_defs = [{'headerName':'Start','field':'start_date', 'cellDataType': 'dateString', 'checkboxSelection': True},
                {'headerName':'Typ','field':'event_type'},
                {'headerName':'Titel','field':'title'},
                {'headerName':'URL','field':'url'},
@@ -64,8 +64,8 @@ column_defs = [{'headerName':'Start','field':'start_date', 'cellDataType': 'date
                {'headerName':'regelmäßig','field':'periodic', 'cellDataType': 'boolean'},
               ]
 
-grid_options = {
-    'columnDefs' : column_defs,
+draft_grid_options = {
+    'columnDefs' : draft_column_defs,
     'rowSelection': 'multiple',
     'suppressRowClickSelection': True,
     'defaultColDef': {
@@ -78,8 +78,8 @@ grid_options = {
     }
 }
 
-grid_draft = Grid(grid_data=events_list2df([]),
-         grid_options=grid_options,
+draft_grid = Grid(grid_data=events_list2df([]),
+         grid_options=draft_grid_options,
          theme='ag-theme-balham-dark',
          export_mode="auto",
          quick_filter=True,
@@ -92,31 +92,83 @@ grid_draft = Grid(grid_data=events_list2df([]),
 # grid_draft
 
 # %%
-# generate grid_draft buttons
+# generate buttons for final grid
 
-delete_rows_button = Button(description='delete selected', layout = button_layout)
-add_row_button = Button(description='add event', layout = button_layout)
-empty_button = Button(description='empty grid', layout = button_layout)
-grid_buttons = HBox([delete_rows_button, add_row_button, empty_button])
+draft_button_add = Button(description='Neues Event', layout = button_layout)
+draft_button_to_final = Button(description='Nach unten', layout = button_layout)
+draft_button_delete = Button(description='Auswahl löschen', layout = button_layout)
+draft_button_empty = Button(description='Tabelle löschen', layout = button_layout)
+draft_buttons = HBox([draft_button_add, draft_button_to_final, draft_button_delete, draft_button_empty])
+
+
+# %%
+# generate final grid widget
+
+final_column_defs = [{'headerName':'Start','field':'start_date', 'cellDataType': 'dateString', 'checkboxSelection': True},
+               {'headerName':'Typ','field':'event_type'},
+               {'headerName':'Titel','field':'title'},
+               {'headerName':'URL','field':'url'},
+               {'headerName':'Organisation','field':'organiser'},
+               {'headerName':'Ort','field':'location'},
+               {'headerName':'regelmäßig','field':'periodic', 'cellDataType': 'boolean'},
+              ]
+
+final_grid_options = {
+    'columnDefs' : final_column_defs,
+    'rowSelection': 'multiple',
+    'suppressRowClickSelection': True,
+    'defaultColDef': {
+        'sortable': True,
+        'editable': True,
+        'resizable': True,
+    # 'enableColResize': True,
+    # 'enableFilter': True,
+    # 'enableRangeSelection': True,
+    }
+}
+
+final_grid = Grid(grid_data=events_list2df([]),
+         grid_options=final_grid_options,
+         theme='ag-theme-balham-dark',
+         export_mode="auto",
+         quick_filter=True,
+         sync_on_edit=True,
+         sync_grid=True,
+         columns_fit='size_to_fit',
+         show_toggle_edit=True,
+         # show_toggle_delete=True,
+)
+
+# final_grid
+
+# %%
+# generate buttons for final grid
+
+final_button_add = Button(description='Neue Veranstaltung', layout = button_layout)
+final_button_delete = Button(description='Auswahl löschen', layout = button_layout)
+final_buttons = HBox([final_button_add, final_button_delete])
 
 # %%
 # display the app
 
 app = VBox([
     date_and_scrapers,
-    grid_draft,
-    grid_buttons
+    draft_grid,
+    draft_buttons,
+    final_grid,
+    final_buttons
     ])
 
 app
 
 
 # %%
-# add functionality to widgets
+# add functionality to date and scraper widgets
 
 def open_url_func(url):
     def open_url(button):
         webbrowser.open_new_tab(url)
+        button.button_style = 'sucess'
     return open_url
 
 def add_scraped_events_to_grid_draft(scraper):
@@ -126,40 +178,58 @@ def add_scraped_events_to_grid_draft(scraper):
         button.button_style = 'info'
         events = scraper.scrape_events(start_datetime, end_datetime)
         events_df = events_list2df(events)
-        grid_draft.get_grid()
-        df_old = grid_draft.grid_data_out['grid']
+        draft_grid.get_grid()
+        df_old = draft_grid.grid_data_out['grid']
         df_new = pd.concat([df_old, events_df], ignore_index=True)
-        grid_draft.update_grid_data(df_new)
+        draft_grid.update_grid_data(df_new)
         button.button_style = 'success'
     return add_scraped_events
-
-def change_color(button):
-    button.button_style = 'success' 
 
 for i, widget_row in enumerate(scraper_widget_rows):
     url_button = widget_row['open_url']
     scraper_button = widget_row['start_scraper']
     url_button.on_click(open_url_func(scrapers_list[i].url))
-    url_button.on_click(change_color)
-    scraper_button.on_click(change_color)
     scraper_button.on_click(add_scraped_events_to_grid_draft(scrapers_list[i]))
 
-def add_row(button):
-    grid_draft.get_grid()
-    df_old = grid_draft.grid_data_out['grid']
-    start_datetime = datetime.combine(date_widgets['start'].value, datetime.min.time())
-    df_new = pd.concat([df_old, events_list2df([Event(title = "TITEL", start_date = start_datetime, url = "www.gib_mir_einen_link.de")])], ignore_index=True)
-    grid_draft.update_grid_data(df_new)
 
-add_row_button.on_click(add_row)
+# %%
+# add functionality to draft and final grid buttos
 
-def delete_rows(button):
-    grid_draft.delete_selected_rows()
+def add_row(grid: Grid):
+    def add_row(button: Button):
+        grid.get_grid()
+        df_old = grid.grid_data_out['grid']
+        start_datetime = datetime.combine(date_widgets['start'].value, datetime.min.time())
+        df_new = pd.concat([df_old, events_list2df([Event(title = "TITEL", start_date = start_datetime, url = "www.gib_mir_einen_link.de")])], ignore_index=True)
+        grid.update_grid_data(df_new)
+    return add_row
 
-delete_rows_button.on_click(delete_rows)
+def delete_rows(grid: Grid):
+    def delete_rows(button: Button):
+        grid.delete_selected_rows()
+    return delete_rows
 
+def empty(grid: Grid):
+    def empty(button: Button):
+        grid.update_grid_data(events_list2df([]))
+    return empty
 
-def empty_grid(button):
-    grid_draft.update_grid_data(events_list2df([]))
+def move_selection(from_grid: Grid, to_grid: Grid):
+    def move_selection(button: Button):
+        from_grid.get_grid()
+        to_grid.get_grid()
+        df_selection = from_grid.grid_data_out['rows']
+        df_to_grid = to_grid.grid_data_out['grid']
+        df_combined = pd.concat([df_to_grid, df_selection], ignore_index=True)
+        to_grid.update_grid_data(df_combined)
+    return move_selection
 
-empty_button.on_click(empty_grid)
+draft_button_add.on_click(add_row(draft_grid))
+draft_button_delete.on_click(delete_rows(draft_grid))
+draft_button_empty.on_click(empty(draft_grid))
+draft_button_to_final.on_click(move_selection(draft_grid, final_grid))
+
+final_button_add.on_click(add_row(final_grid))
+final_button_delete.on_click(delete_rows(final_grid))
+
+# %%
